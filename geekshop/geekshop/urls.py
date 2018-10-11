338877ed726ffb3f393707b915b8796576e226c6
coola4kov <light.lds@gmail.com>
2018-10-11 21:04:52 +0500

    updated import of app

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from mainapp import views
+import mainapp.views as mainapp
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalogue/', views.catalogue, name='catalogue'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('', views.index, name='index'),
+    path('catalogue/', mainapp.catalogue, name='catalogue'),
+    path('contacts/', mainapp.contacts, name='contacts'),
+    path('', mainapp.index, name='index'),
 
 ]
