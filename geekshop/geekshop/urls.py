679e56bf688b31f696dac1ce66f00f97634857f2
coola4kov <light.lds@gmail.com>
2018-10-17 01:05:55 +0500

    added Media URL, added models, added data into db, created admin.

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -14,14 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
-import mainapp.views as mainapp
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
+import mainapp.views as mainapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalogue/', mainapp.catalogue, name='catalogue'),
     # path('contacts/', mainapp.contacts, name='contacts'),
     path('', mainapp.index, name='index'),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name="Название", max_length=32, unique=True)
+    description = models.TextField(verbose_name="Краткое описание", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Catalogue(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name="Название продукта", max_length=200, unique=True)
+    image = models.ImageField(verbose_name="Изображение продукта", upload_to='products_images', blank=True)
+    short_desc = models.CharField(verbose_name="Краткое описание", max_length=60, blank=True)
+    description = models.TextField(verbose_name="Детальное описание", blank=True)
+    price = models.DecimalField(verbose_name="Текущая цена", max_digits=8, decimal_places=2, default=0)
+    old_price = models.DecimalField(verbose_name="Старая цена", max_digits=8, decimal_places=2, default=0)
+    sale = models.PositiveIntegerField(verbose_name="Скида", default=0)
+    new_product = models.BooleanField(verbose_name="Новый товар", default=False)
+    storage = models.PositiveIntegerField(verbose_name="Остатки на складе", default=0)
+
+    def __str__(self):
+        return "{} ({})".format(self.name, self.category.name)
