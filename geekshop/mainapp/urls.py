3db16339341cfe0d009f2efa0ef1211676f51f0c
coola4kov <light.lds@gmail.com>
2019-05-04 22:48:17 +0500

    Class based view for mainapp

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from mainapp.views import catalogue_detail, catalogue, product_detail
+from mainapp.views import CatalogueView, CatalogueCategoryView, ProductView
 
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', catalogue, name='catalogue'),
-    path('<int:category_id>/', catalogue_detail, name='catalogue_detail'),
-    path('product/<int:product_id>/', product_detail, name='product_detail')
+    path('',CatalogueView.as_view(), name='catalogue'),
+    path('<int:pk>/', CatalogueCategoryView.as_view(), name='catalogue_category'),
+    path('product/<int:pk>/', ProductView.as_view(), name='catalogue_detail'),
+    # path('product/<int:pk>/', product_detail, name='product_detail')
+
 ]
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic.list import MultipleObjectMixin, ListView
+from django.views.generic.detail import DetailView
+
 from .models import Catalogue
 
-
 import random
-
 
 
 def index(request):
@@ -14,59 +14,44 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def catalogue(request):
-    new_prod = Catalogue.objects.filter(new_product=True)
-    catalogue = Catalogue.objects.all()
-    paginator = Paginator(catalogue, 3)
-    page = request.GET.get('page')
-    try:
-        prod_paginator = paginator.page(page)
-    except PageNotAnInteger:
-        prod_paginator = paginator.page(1)
-    except EmptyPage:
-        prod_paginator = paginator.page(paginator.num_pages)
-    if len(new_prod) < 2:
-        new = []
-    else:
-        new = random.sample(list(new_prod), 2)
-    context = {
-        'title': 'каталог',
-        "catalogue": prod_paginator,
-        "new": new,
-    }
-    return render(request, 'mainapp/catalogue.html', context)
+class ProductView(DetailView):
+    model = Catalogue
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductView, self).get_context_data(**kwargs)
+        context['title'] = 'каталог'
+        return context
 
 
-def catalogue_detail(request, category_id=1):
-    catalogue_objects = get_list_or_404(Catalogue, category_id=category_id)
-    new_prod = Catalogue.objects.filter(new_product=True, category_id=category_id)
-    paginator = Paginator(catalogue_objects, 3)
-    page = request.GET.get('page')
-    try:
-        prod_paginator = paginator.page(page)
-    except PageNotAnInteger:
-        prod_paginator = paginator.page(1)
-    except EmptyPage:
-        prod_paginator = paginator.page(paginator.num_pages)
-    if len(new_prod) < 2:
-        new = []
-    else:
-        new = random.sample(list(new_prod), 2)
-    context = {
-        'title': 'каталог',
-        "catalogue": prod_paginator,
-        "new": new,
-    }
-    return render(request, 'mainapp/catalogue.html', context)
+class CatalogueView(ListView, MultipleObjectMixin):
+    model = Catalogue
+    template_name = 'mainapp/catalogue.html'
+    paginate_by = 3
+    allow_empty = False
+
+    @property
+    def category_id(self):
+        return self.kwargs.get('pk')
+
+    def get_new_products(self):
+        new_prod = Catalogue.objects.filter(new_product=True, category_id=self.category_id)
+        if len(new_prod) < 2:
+            new = []
+        else:
+            new = random.sample(list(new_prod), 2)
+        return new
+
+    def get_context_data(self, **kwargs):
+        new = self.get_new_products()
+        context = super(CatalogueView, self).get_context_data(new=new, **kwargs)
+        context['title'] = 'каталог'
+        return context
 
 
-def product_detail(request, product_id=None):
-    product_object = get_object_or_404(Catalogue, id=product_id)
-    context = {
-        'title': 'каталог',
-        'product': product_object
-    }
-    return render(request, 'mainapp/product.html', context)
+class CatalogueCategoryView(CatalogueView):
+
+    def get_queryset(self):
+        return get_list_or_404(Catalogue, category_id=self.category_id)
 
 
 def contacts(request):
